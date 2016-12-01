ec435fc67221f0d898b1dd20b47df61106ecda72
Peng.Li <nowind_lee@qq.com>
2016-12-01 12:13:03 +0800

    get request info

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-Servlet Demo
-============
+Get Request Info in Servlet Demo
+===============================
 
-A simplest servlet demo.
+How to get request information in a servlet.
 
 ```
 mvn jetty:run
 ```
+
+Then visit <http://localhost:8080/mycontext/hello/world?name=111>, and see the information on the page.
+
+```
+RequestURL: http://localhost:8080/mycontext/hello/world
+Scheme: http
+Protocol: HTTP/1.1
+ContextPath: /mycontext
+ServletPath: /hello
+Method: GET
+PathInfo: /world
+QueryString: name=111
+------------------ parameters ------------------
+name : 111
+RequestedSessionId: oty91ifwybfe1cyfexanc6gou
+CharacterEncoding: null
+ContentType: null
+RemoteUser: null
+RemoteAddr: 0:0:0:0:0:0:0:1
+RemotePort: 58602
+RemoteHost: 0:0:0:0:0:0:0:1
+LocalAddr: 0:0:0:0:0:0:0:1
+LocalName: 0:0:0:0:0:0:0:1
+LocalPort: 8080
+ServerName: localhost
+```
+
+Note:
+- `contextPath` is defined in `pom.xml`
+
+
--- src/main/java/demo/HelloWorldServlet.java
+++ src/main/java/demo/HelloWorldServlet.java
@@ -1,16 +1,71 @@
 package demo;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("Hello, servlet");
+        List<String> lines = new ArrayList<String>();
+
+        lines.add("RequestURL: " + req.getRequestURL());
+
+        lines.add("Scheme: " + req.getScheme());
+        lines.add("Protocol: " + req.getProtocol());
+        lines.add("ContextPath: " + req.getContextPath());
+        lines.add("ServletPath: " + req.getServletPath());
+        lines.add("Method: " + req.getMethod());
+        lines.add("PathInfo: " + req.getPathInfo());
+
+        lines.add("QueryString: " + req.getQueryString());
+        lines.add("------------------ parameters ------------------");
+        Enumeration parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String name = (String) parameterNames.nextElement();
+            String value = req.getParameter(name);
+            lines.add(name + ": " + value);
+        }
+
+        lines.add("RequestedSessionId: " + req.getRequestedSessionId());
+        lines.add("CharacterEncoding: " + req.getCharacterEncoding());
+        lines.add("ContentType: " + req.getContentType());
+
+        lines.add("RemoteUser: " + req.getRemoteUser());
+        lines.add("RemoteAddr: " + req.getRemoteAddr());
+        lines.add("RemotePort: " + req.getRemotePort());
+        lines.add("RemoteHost: " + req.getRemoteHost());
+
+        lines.add("LocalAddr: " + req.getLocalAddr());
+        lines.add("LocalName: " + req.getLocalName());
+        lines.add("LocalPort: " + req.getLocalPort());
+
+        lines.add("ServerName: " + req.getServerName());
+
+        lines.add("----------------- headers ------------------");
+        Enumeration headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            String value = req.getHeader(headerName);
+            lines.add(headerName + ": " + value);
+        }
+
+        lines.add("----------------- cookies ------------------");
+        for (Cookie cookie : req.getCookies()) {
+            lines.add(cookie.getName() + ": " + cookie.getValue());
+        }
+
+        for (String line : lines) {
+            resp.getWriter().append("<div>" + line + "</div>");
+        }
+
     }
 
 }
